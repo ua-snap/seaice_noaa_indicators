5c9afd67714f6ee3ee4081470c038282ef4c59e6
EarthScientist <lindgren.mike@gmail.com>
2018-05-16 21:13:00 -0800

    add profile selector example and updates...

--- compute_summer_winter_data.py
+++ compute_summer_winter_data.py
@@ -2,16 +2,12 @@ def get_summer(month):
 	return (month == 8) | (month == 9)
 def get_winter(month):
 	return (month == 1) | (month == 2)
-# def mean_filter_2D( arr, footprint ):
-# 	from scipy.ndimage import generic_filter
-# 	return generic_filter( arr, np.nanmean, footprint=footprint )
 
 if __name__ == '__main__':
 	import xarray as xr
 	import numpy as np
-	import os
+	import os, dask
 	from functools import partial
-	# import multiprocessing as mp
 
 	# open the NetCDF that we made...
 	fn = '/workspace/Shared/Tech_Projects/SeaIce_NOAA_Indicators/project_data/nsidc_0051/NetCDF/nsidc_0051_sic_nasateam_1978-2017_Alaska.nc'
@@ -20,33 +16,25 @@ if __name__ == '__main__':
 	# slice the data to the full years... currently this is 1979-2016
 	ds = ds.sel( time=slice( '1979', '2016' ) )
 
-	# # how to perform a spatial 2D mean filter on the data... (without any aggregation)
-	# footprint = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])
-	# f = partial( mean_filter_2D, footprint=footprint )
-
-	# arr = ds.sic.data.copy()
-	# ind = np.where( arr > 1 )
-	# arr[ ind ] = np.nan
-
-	# pool = mp.Pool( 32 )
-	# done = np.array( pool.map( f, arr ) )
-	# pool.close()
-	# pool.join()
-	# done[ ind ] = ds.sic.data[ ind ]
-
-	# # put the newly smoothed data to the existing xarray object
-	# ds['sic'] = (('time', 'yc', 'xc'), done )
-
-	# make climatology?
-	# ds_day_clim = ds.groupby( 'time.day' ).mean()
-
+	# # make climatology? --> 0-366 includes leaps --> this may make more sense to read in as a dask array, compute, write out
+	# ds_day_clim = ds.groupby( 'time.dayofyear' ).mean( dim='time' ).compute()
 
 	# get the summer and winter seasons htat were determined in table 1 in the paper
-	summer = ds.sel( time=get_summer( ds[ 'time.month' ] ) )
-	winter = ds.sel( time=get_winter( ds[ 'time.month' ] ) )
+	summer = ds['sic'].sel( time=get_summer( ds[ 'time.month' ] ) )
+	winter = ds['sic'].sel( time=get_winter( ds[ 'time.month' ] ) )
+
+	summer_ind = np.where( summer.data )
+	winter_ind = np.where( winter.data )
+
+	summer_oob_vals = summer[ summer_ind ]
+	winter_oob_vals = winter[ winter_ind ]
 
 	# get the means and standard deviations of these season aggregates
-	summer_mean = summer[ 'sic' ].groupby( 'time.year' ).mean( 'time' )
-	summer_std = summer[ 'sic' ].groupby( 'time.year' ).std( 'time' )
-	winter_mean = winter[ 'sic' ].groupby( 'time.year' ).mean( 'time' )
-	winter_std = winter[ 'sic' ].groupby( 'time.year' ).std( 'time' )
+	summer_mean = summer[ 'sic' ].groupby( 'time.year' ).mean( dim='time' )
+	summer_std = summer[ 'sic' ].groupby( 'time.year' ).std( dim='time' )
+	winter_mean = winter[ 'sic' ].groupby( 'time.year' ).mean( dim='time' )
+	winter_std = winter[ 'sic' ].groupby( 'time.year' ).std( dim='time' )
+
+	# [ NOTE ]: to make an ordinal date that matches the somewhat asinine epoch used by matlab in python
+	# ordinal_date = date.toordinal(date(1971,1,1)) + 366 
+	# if not the number will be 366 days off due to the epoch starting January 0, 0000 whereas in Py Jan 1, 0001.
---
+++ select_profile_using_xy.py
@@ -0,0 +1,21 @@
+# sample point
+
+import geopandas as gpd
+from shapely.geometry import Point
+import ast, rasterio
+
+x,y = (-1915085, 937121)
+pt = Point(x,y)
+
+gdf = gpd.GeoDataFrame( {'id':[1],'geometry':[pt]}, crs={'init':'epsg:3411'}, geometry='geometry' )
+gdf.to_file( '/workspace/Shared/Tech_Projects/SeaIce_NOAA_Indicators/project_data/nsidc_0051/test_point_ak.shp' )
+
+a = Affine(*ast.literal_eval(ds.affine_transform)[:6])
+col, row = ~a * (x, y)
+row, col = np.array([row,col]).astype(int)
+
+# rst = rasterio.open('/workspace/Shared/Tech_Projects/SeaIce_NOAA_Indicators/project_data/nsidc_0051/GTiff/alaska/1978/nt_19781026_n07_v1-1_n.tif')
+# arr = rst.read(1)
+# arr[row,col] #<- select a single profile for testing
+
+profile = ds.sic[:,row,col]
