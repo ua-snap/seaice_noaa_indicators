1cd14905b86aa3f1d118d1a62da98f2b3303fe46
EarthScientist <lindgren.mike@gmail.com>
2018-11-01 15:30:59 -0800

    updates for pipeline run

--- make_figure_7_paper.py
+++ make_figure_7_paper.py
@@ -11,16 +11,31 @@ import geopandas as gpd
 from affine import Affine
 import numpy as np
 import rasterio, os, calendar, datetime
+import argparse
 
+# parse some args
+parser = argparse.ArgumentParser( description='stack the hourly outputs from raw WRF outputs to NetCDF files of hourlies broken up by year.' )
+parser.add_argument( "-b", "--base_path", action='store', dest='base_path', type=str, help="input hourly directory containing the NSIDC_0051 data converted to GTiff" )
+parser.add_argument( "-w", "--window_len", action='store', dest='window_len', type=int, help="window length to add to the output NetCDF file name" )
+
+# unpack args
+args = parser.parse_args()
+base_path = args.base_path
+window_len = args.window_len
+
+# window_len = 4
 
 # read in the FUBU dates for all years
-window_len = 4
-fubu_fn = '/workspace/Shared/Tech_Projects/SeaIce_NOAA_Indicators/project_data/nsidc_0051/NetCDF/nsidc_0051_sic_nasateam_1978-2017_Alaska_hann_{}_fubu_dates.nc'.format(window_len)
+# handle custom hann
+if window_len == 1:
+	window_len = 'paper_weights'
+
+fubu_fn = os.path.join( base_path,'NetCDF','nsidc_0051_sic_nasateam_1978-2017_Alaska_hann_{}_fubu_dates.nc'.format(window_len))
 ds = xr.open_dataset( fubu_fn )
 a = Affine(*eval( ds.affine_transform )[:6]) # make an affine transform for lookups
 
 # make barrow points and get their row/col locs
-points_fn = '/workspace/Shared/Tech_Projects/SeaIce_NOAA_Indicators/project_data/selection_points/Barrow_points_bestfit.shp'
+points_fn = os.path.join(base_path,'selection_points','barrow_points.shp')
 points = gpd.read_file( points_fn ).geometry.apply(lambda x: (x.x, x.y)).tolist()
 colrows = [ ~a*pt for pt in points ]
 colrows = [ (int(c),int(r)) for c,r in colrows ]
@@ -57,7 +72,7 @@ for group in metric_groups:
 
 	# trend.plot(kind='line', ax=ax)
 
-	plt.savefig('/workspace/Shared/Tech_Projects/SeaIce_NOAA_Indicators/project_data/selection_points/barrow_avg_hann_{}_{}-{}_fig7a.png'.format(window_len, start, end), figsize=(20,2), dpi=300)
+	plt.savefig( os.path.join('png','barrow_avg_hann_{}_{}-{}_fig7a.png'.format(window_len, start, end)), figsize=(20,2), dpi=300)
 	plt.cla()
 	plt.close()
 
--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -5,25 +5,28 @@ import subprocess, os
 os.chdir('/atlas_scratch/malindgren/repos/seaice_noaa_indicators/pipeline')
 
 base_path = '/atlas_scratch/malindgren/nsidc_0051'
-window_len = 'paper_weights'
+# window_len = 'paper_weights'
+window_len = 4
 ncpus = 64
-
-_ = subprocess.call(['ipython','make_daily_timeseries_interp.py','--','-b', base_path, '-w', window_len])
+_ = subprocess.call(['ipython','make_daily_timeseries_interp.py','--','-b', base_path, '-w', str(window_len)])
 
 # make clim
-fn = '/atlas_scratch/malindgren/nsidc_0051/NetCDF/nsidc_0051_sic_nasateam_1978-2017_Alaska_hann_{}.nc'.format(window_len)
-out_fn = '/atlas_scratch/malindgren/nsidc_0051/NetCDF/nsidc_0051_sic_nasateam_1979-2017_Alaska_hann_{}_climatology.nc'.format(window_len)
+fn = '/atlas_scratch/malindgren/nsidc_0051/NetCDF/nsidc_0051_sic_nasateam_1978-2017_Alaska_hann_{}.nc'.format(str(window_len))
+out_fn = '/atlas_scratch/malindgren/nsidc_0051/NetCDF/nsidc_0051_sic_nasateam_1979-2017_Alaska_hann_{}_climatology.nc'.format(str(window_len))
 begin = '1979'
 end = '2017'
 _ = subprocess.call(['ipython','make_daily_timeseries_climatology.py','--','-f', fn, '-o', out_fn, '-b', begin, '-e', end])
 
 # calc FUBU
-fn = os.path.join( base_path, 'NetCDF','nsidc_0051_sic_nasateam_1978-2017_Alaska_hann_{}.nc'.format(window_len)
+fn = os.path.join( base_path, 'NetCDF','nsidc_0051_sic_nasateam_1978-2017_Alaska_hann_{}.nc'.format(str(window_len)))
 _ = subprocess.call(['ipython','compute_fubu_allyears_alaska_domain.py','--','-b', base_path, '-f', fn, '-begin', begin, '-end', end])
 
 # calc FUBU clim
-fn = os.path.join( base_path,'NetCDF','nsidc_0051_sic_nasateam_1979-2017_Alaska_hann_{}_climatology.nc'.format(window_len) )
+fn = os.path.join( base_path,'NetCDF','nsidc_0051_sic_nasateam_1979-2017_Alaska_hann_{}_climatology.nc'.format(str(window_len)) )
 _ = subprocess.call(['ipython','compute_fubu_allyears_alaska_domain_climatology.py','--','-b', base_path, '-f', fn, '-n', str(ncpus)])
 
-
 # plot this stuff.
+_ = subprocess.call(['ipython','make_figure_3_paper.py','--','-b', base_path, '-w', str(window_len) ])
+_ = subprocess.call(['ipython','make_figure_4_paper.py','--','-b', base_path, '-w', str(window_len) ])
+_ = subprocess.call(['ipython','make_figure_5-6_paper.py','--','-b', base_path, '-w', str(window_len) ])
+_ = subprocess.call(['ipython','make_figure_7_paper.py','--','-b', base_path, '-w', str(window_len) ])
